077593e67400ad5cf652f031d1291fbdeb548b6b
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-06 21:57:58 -0700

    s

--- Scripts/Setup-Env.sh
+++ Scripts/Setup-Env.sh
@@ -5,51 +5,21 @@ if [[ $UID -ne 0 ]] && ! sudo grep -q "$USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoer
     echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 fi 
 
-if [[ -z $(sudo ls /root/WZhu 2>/dev/null) ]]; then
-    echo "Create symbolic link /root/WZhu"
-    sudo ln -s $HOME/WZhu /root/WZhu
-fi 
-
-if ! grep -q "WZhu Env PATH" ~/.bashrc; then
+if ! grep -q "WZhu Env Setup" ~/.bashrc; then
     echo ' 
-# === WZhu Env PATH ===
-export PATH="$PATH:$HOME/WZhu/Scripts"' >> ~/.bashrc
-fi 
-
-if ! sudo grep -q "WZhu Env PATH" /root/.bashrc; then
-    echo '
-# === WZhu Env PATH ===
-export PATH="$PATH:$HOME/WZhu/Scripts"' | sudo tee -a /root/.bashrc >/dev/null 
-fi 
-
-if [ ! -f ~/.bashrc ] || ! grep -q "WZhu Func Loader" ~/.bashrc; then
-    echo '
-# === WZhu Func Loader ===
-function Load-Micro-Functions {
-    if [[ -f ~/WZhu/Scripts/MicroFunctions.sh ]]; then 
-        source ~/WZhu/Scripts/MicroFunctions.sh
-    fi 
-}' >> ~/.bashrc
-fi 
-
-if ! sudo grep -q "WZhu Func Loader" /root/.bashrc; then
-    echo '
-# === WZhu Func Loader ===
-function Load-Micro-Functions {
-    if [[ -f $HOME/WZhu/Scripts/MicroFunctions.sh ]]; then 
-        source $HOME/WZhu/Scripts/MicroFunctions.sh
-    fi 
-}' | sudo tee -a /root/.bashrc >/dev/null 
-fi 
-
-if [ ! -f ~/.bashrc ] || ! grep -q "WZhu Perforce Env" ~/.bashrc; then
-    echo '
-# === WZhu Perforce Env ===
+# === WZhu Env Setup ===
+export PATH="$PATH:$HOME/WZhu/Scripts"
 export P4PORT=p4proxy-sc.nvidia.com:2006
 export P4USER=wanliz
 export P4CLIENT=wanliz-sw-gpu-driver-home
-export P4IGNORE=$HOME/.p4ignore' >> ~/.bashrc
-fi
+export P4IGNORE=$HOME/.p4ignore
+
+function Load-Micro-Functions {
+    if [[ -f ~/WZhu/Scripts/Micro-Functions.sh ]]; then 
+        source ~/WZhu/Scripts/Micro-Functions.sh
+    fi 
+}' >> ~/.bashrc
+fi 
 
 if [ ! -f ~/.p4ignore ]; then 
     echo "_out
@@ -61,6 +31,7 @@ if [ ! -f ~/.p4ignore ]; then
 fi 
 
 # Install package dependencies
+# Run `QT_DEBUG_PLUGINS=1 p4v` to debug if p4v crashed
 for pkg in libopengl0 libnss3 libxkbfile1 \
     libxcb-cursor0 libxcb-icccm4 libxcb-image0 \
     libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 \
@@ -68,8 +39,6 @@ for pkg in libopengl0 libnss3 libxkbfile1 \
     libxcb-shape0 libxcb1-dev; do 
     dpkg -s $pkg &>/dev/null || sudo apt install -y $pkg 
 done 
-
-# Run `QT_DEBUG_PLUGINS=1 p4v` to debug if p4v crashed
 
 if ! dpkg --print-foreign-architectures | grep -q '^i386$'; then
     echo "Enabling i386 architecture"
