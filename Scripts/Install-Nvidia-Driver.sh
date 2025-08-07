278703463825cbaffa97cb0e018a696c4cdba14e
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-06 23:07:19 -0700

    s

--- Scripts/Install-Nvidia-Driver.sh
+++ Scripts/Install-Nvidia-Driver.sh
@@ -1,49 +1,43 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-    echo "Please run as root"
-    echo "Aborting!"
-    exit 1
-fi 
-
 # Mount /mnt/linuxqa
 if [[ ! -z $(ls /mnt/linuxqa 2>/dev/null) ]]; then
-    apt install -y nfs-common
-    mkdir -p /mnt/linuxqa
-    mount linuxqa.nvidia.com:/storage/people /mnt/linuxqa || exit 1
+    sudo apt install -y nfs-common
+    sudo mkdir -p /mnt/linuxqa
+    sudo mount linuxqa.nvidia.com:/storage/people /mnt/linuxqa || exit 1
 fi 
 
 # Mount /mnt/builds
 if [[ ! -z $(ls /mnt/builds 2>/dev/null) ]]; then
-    apt install -y nfs-common
-    mkdir -p /mnt/builds
-    mount linuxqa.nvidia.com:/storage3/builds /mnt/builds  
+    sudo apt install -y nfs-common
+    sudo mkdir -p /mnt/builds
+    sudo mount linuxqa.nvidia.com:/storage3/builds /mnt/builds  
 fi 
 
 # Mount /mnt/dvsbuilds
 if [[ ! -z $(ls /mnt/dvsbuilds 2>/dev/null) ]]; then
-    apt install -y nfs-common
-    mkdir -p /mnt/dvsbuilds
-    mount linuxqa.nvidia.com:/storage5/dvsbuilds /mnt/dvsbuilds
+    sudo apt install -y nfs-common
+    sudo mkdir -p /mnt/dvsbuilds
+    sudo mount linuxqa.nvidia.com:/storage5/dvsbuilds /mnt/dvsbuilds
 fi 
 
 # Mount /mnt/data
 if [[ ! -z $(ls /mnt/data 2>/dev/null) ]]; then
-    apt install -y nfs-common
-    mkdir -p /mnt/data
-    mount linuxqa.nvidia.com:/storage/data /mnt/data 
+    sudo apt install -y nfs-common
+    sudo mkdir -p /mnt/data
+    sudo mount linuxqa.nvidia.com:/storage/data /mnt/data 
 fi 
 
 # Sync /root/nvt
 if [[ ! -z $(ls /root/nvt 2>/dev/null) ]]; then
-    apt install -y python3 python3-pip libjpeg-dev 
-    /mnt/linuxqa/nvt.sh sync || exit 1
+    sudo apt install -y python3 python3-pip libjpeg-dev 
+    sudo su -c "/mnt/linuxqa/nvt.sh sync" || exit 1
 fi 
 
 if [[ -z $1 ]]; then
-    /mnt/linuxqa/nvt.sh driver --help 
+    sudo su -c "/mnt/linuxqa/nvt.sh driver --help"
 else 
-    /mnt/linuxqa/nvt.sh driver $@ 
+    sudo su -c "/mnt/linuxqa/nvt.sh driver $@"
     read -p "ENVVARS: " envvars
     for pair in $envvars; do 
         echo "export ${pair%%=*}=${pair#*=}"
