3260c64329c662264cb02ccdfe56ccbdc785e9d2
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-06 21:34:24 -0700

    s

--- Scripts/Micro-Functions.sh
+++ Scripts/Micro-Functions.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+function Unsandbag-NvTest-Driver {
+    driver=$1
+    if [[ "$1" == "http"* ]]; then 
+        driver=/root/nvt/driver/$(basename "$1")
+    fi 
+    pushd /tmp >/dev/null 
+        sudo rm -rf /tmp/tests-Linux-$(uname -m)
+        tar -xf $(dirname $driver)/tests-Linux-$(uname -m).tar
+        ./tests-Linux-aarch64/sandbag-tool/sandbag-tool -unsandbag
+        ./tests-Linux-aarch64/sandbag-tool/sandbag-tool -print 
+    popd >/dev/null 
+}
