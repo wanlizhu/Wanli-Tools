551c14a2601bff7b7f3102a53c886c743f197f41
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-06 23:24:29 -0700

    s

--- Scripts/Install-Nvidia-Driver.sh
+++ Scripts/Install-Nvidia-Driver.sh
@@ -9,9 +9,9 @@ if [[ ! -z $(ls /root/nvt 2>/dev/null) ]]; then
 fi 
 
 if [[ -z $1 ]]; then
-    sudo su -c "bash -c \"/mnt/linuxqa/nvt.sh driver --help\""
+    sudo su -c "/mnt/linuxqa/nvt.sh driver --help"
 else 
-    sudo su -c "bash -c \"/mnt/linuxqa/nvt.sh driver $@\"" || exit 1
+    sudo su -c "/mnt/linuxqa/nvt.sh driver \"$@\"" || exit 1
     read -p "ENVVARS: " envvars
     for pair in $envvars; do 
         echo "export ${pair%%=*}=${pair#*=}"
