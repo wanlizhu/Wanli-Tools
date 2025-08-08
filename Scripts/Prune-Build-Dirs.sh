2db122d963e0f23da43b4eb77b639e7f98abcba8
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-08 12:25:08 -0700

    s

--- Scripts/Prune-Build-Dirs.sh
+++ Scripts/Prune-Build-Dirs.sh
@@ -5,28 +5,37 @@ rootDir=/media/wanliz/data/wanliz-sw-gpu-driver-home
 outputFile=/media/wanliz/data/wanliz-sw-gpu-driver-home/rel/gpu_drv/r580/r580_00/_out/Linux_amd64_develop/NVIDIA-Linux-x86_64-*-internal.run
 buildScript=$HOME/WZhu/Scripts/NvMake.sh
 
+Cleanup-and-Restore() {
+    echo "Cleanup and restore *-hide-and-checking dirs"
+    find "$rootDir" -type d -name '*-hide-and-checking' \
+        -exec bash -c 'for d; do mv "$d" "${d%-hide-and-checking}"; done' _ {} +
+}
+trap Cleanup-and-Restore EXIT INT TERM
+
 Run-Build-Script() {
     export P4ROOT=$rootDir
-    bash $buildScript sweep <<< "" &>/dev/null 
-    bash $buildScript <<< "" &>/dev/null 
+    bash $buildScript sweep <<< "" 
+    bash $buildScript <<< "" 
     if (( $? != 0 )) || [[ -z $(ls $outputFile 2>/dev/null) ]]; then return 1; fi
     return 0
 }
 
-Hide-Dir-And-Test-Build() {
-    dirPath=$1
-    if [[ $dirPath == *"_out"* || 
-        $dirPath == *"-tobe-removed"* ]]; then
+Hide-Dir-and-Test-Build() {
+    dirPath="$1"
+    if [[ "$dirPath" == *"_out"* || 
+        "$dirPath" == *"-hide-and-checked"* ||
+        "$dirPath" == *"-hide-and-checking"* ]]; then
         return 0
     fi
 
-    mv $dirPath $dirPath-tobe-removed 
+    mv "$dirPath" "$dirPath-hide-and-checking"
 
     if Run-Build-Script; then
-        echo "[Found] $dirPath"
+        mv "$dirPath-hide-and-checking" "$dirPath-hide-and-checked"
+        echo "[Found] $dirPath" | tee -a hide-and-checked.txt
     else
-        mv $dirPath-tobe-removed $dirPath
-        Process-Children-Dirs $dirPath
+        mv "$dirPath-hide-and-checking" "$dirPath"
+        Process-Children-Dirs "$dirPath"
     fi
 }
 
@@ -36,7 +45,8 @@ Process-Children-Dirs() {
         child="${child%/}"
         [[ ! -d "$child" || 
             $child == *"_out"* ||
-            $child == *"-tobe-removed"* ]] && continue
+            $child == *"-hide-and-checked"*
+            $child == *"-hide-and-checking"* ]] && continue
         Hide-Dir-And-Test-Build "$child"
     done
 }
@@ -47,4 +57,5 @@ echo "Output File  : $outputFile"
 echo "Build Script : $buildScript"
 read -p "Press [Enter] to continue: "
 
-Process-Children-Dirs $rootDir
+rm -rf hide-and-checked.txt
+Process-Children-Dirs "$rootDir"
