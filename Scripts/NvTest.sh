4e42a63e1615a7346a5afd0cb9c5cb83c2b315d6
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-14 15:40:53 -0700

    s

--- Scripts/NvTest.sh
+++ Scripts/NvTest.sh
@@ -74,20 +74,16 @@ elif [[ $1 == startx ]]; then
     fi 
     echo "Xorg PID: $(pidof Xorg)"
     xrandr | grep current
-else
-
-    if [[ $1 == viewperf ]]; then 
-        if [[ -z $2 ]]; then echo "viewset name is missing"; exit 1; fi 
-        if [[ $WZHU_PI == 1 ]]; then 
-            commandLine="cd $(pwd) && $HOME/SinglePassCapture/pic-x --api=ogl --check_clocks=0 --sample=24000 --aftbuffersize=2048 --name=viewperf-$2-subtest$3-on-$(hostname) --startframe=100 --exe=./viewperf/bin/viewperf --arg=\"viewsets/$2/config/$2.xml $3 -resolution 3840x2160\" --workdir=/root/nvt/tests/viewperf2020v3/viewperf2020 | grep -v \"won't hook API\"" 
-        else
-            commandLine="cd /root/nvt/tests/viewperf2020v3/viewperf2020 && ./viewperf/bin/viewperf viewsets/$2/config/$2.xml $3 -resolution 3840x2160 && cat /root/nvt/tests/viewperf2020v3/viewperf2020/results/$2*/results.xml"
-        fi 
-
-        echo "${commandLine}"
-        read -p "Press [Enter] to continue as root: "
-        sudo -H bash -lc "$(env | grep -E '^__GL_' | while IFS='=' read -r k v; do printf 'export %s=%q; ' $k $v; done)  $commandLine" 
-    else 
-        sudo -H bash -lc "/mnt/linuxqa/nvt.sh $*" 
+elif [[ $1 == viewperf ]]; then 
+    if [[ $WZHU_PI == 1 ]]; then 
+        commandLine="cd $(pwd) && $HOME/SinglePassCapture/pic-x --api=ogl --check_clocks=0 --sample=24000 --aftbuffersize=2048 --name=viewperf-$2-subtest$3-on-$(hostname) --startframe=100 --exe=./viewperf/bin/viewperf --arg=\"viewsets/$2/config/$2.xml $3 -resolution 3840x2160\" --workdir=/root/nvt/tests/viewperf2020v3/viewperf2020 | grep -v \"won't hook API\"" 
+    else
+        commandLine="cd /root/nvt/tests/viewperf2020v3/viewperf2020 && ./viewperf/bin/viewperf viewsets/$2/config/$2.xml $3 -resolution 3840x2160 && cat /root/nvt/tests/viewperf2020v3/viewperf2020/results/$2*/results.xml"
     fi 
+
+    echo "${commandLine}"
+    read -p "Press [Enter] to continue as root: "
+    sudo -H bash -lc "$(env | grep -E '^__GL_' | while IFS='=' read -r k v; do printf 'export %s=%q; ' $k $v; done)  $commandLine" 
+else 
+    sudo -H bash -lc "$(env | grep -E '^__GL_' | while IFS='=' read -r k v; do printf 'export %s=%q; ' $k $v; done) /mnt/linuxqa/nvt.sh $*" 
 fi 
