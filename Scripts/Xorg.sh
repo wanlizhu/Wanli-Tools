fd8175c233f6c0e03498a7e59e58a75538fa873e
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-14 16:13:04 -0700

    s

--- Scripts/Xorg.sh
+++ Scripts/Xorg.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-export DISPLAY=:0
-echo "export DISPLAY=:0"
+descriptor=$(find /tmp/.X11-unix -maxdepth 1 -type s -name 'X*' -printf '%f\n' 2>/dev/null | sed 's/^X//' | sort -n | head -1) 
+if [[ ! -z $descriptor ]]; then 
+    export DISPLAY=":$descriptor"
+    echo "export DISPLAY=:$descriptor"
+fi 
 
 if [[ $1 == vnc ]]; then 
     [[ -z $(pidof Xorg) ]] && { echo "Xorg is not running"; exit 1; }
     [[ ! -e ~/.vnc/passwd ]] && x11vnc -storepasswd
-    screen -S vnc-mirroring x11vnc -display :0 -auth ~/.Xauthority -noshm -forever --loop -noxdamage -repeat -shared
+    screen -S vnc-mirroring x11vnc -display :0 -auth ~/.Xauthority -forever --loop -noxdamage -repeat -shared
     sudo ss -tulpn | grep -E "5900|5901|5902"
 elif [[ $1 == xauth ]]; then 
     xauthPath=$(ps aux | grep '[X]org' | grep -oP '(?<=-auth )[^ ]+')
