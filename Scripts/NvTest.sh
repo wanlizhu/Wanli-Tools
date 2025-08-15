e8b51def50887360603c545d637d8a075b76fe32
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-14 22:09:07 -0700

    s

--- Scripts/NvTest.sh
+++ Scripts/NvTest.sh
@@ -23,18 +23,35 @@ if [[ ! -z $(ls /root/nvt 2>/dev/null) ]]; then
 fi 
 
 if [[ $1 == driver || $1 == drivers ]]; then 
-    if [[ $2 == rsync ]]; then 
-        config=$3
-        rsync -ah --progress wanliz@office:/media/wanliz/data/wanliz-sw-gpu-driver-office/rel/gpu_drv/r580/r580_00/_out/Linux_$(uname -m | sed 's/^x86_64$/amd64/')_${config:-develop}/NVIDIA-Linux-$(uname -m)-*-internal.run wanliz@office:/media/wanliz/data/wanliz-sw-gpu-driver-office/rel/gpu_drv/r580/r580_00/_out/Linux_$(uname -m | sed 's/^x86_64$/amd64/')_${config:-develop}/tests-Linux-$(uname -m).tar $HOME && echo || exit 1
+    if [[ $2 == rsync ]]; then
+        branch=rel/gpu_drv/r580/r580_00
+        module=drivers 
+        config=develop
+        while [[ $# -gt 2 ]]; do 
+            case $3 in 
+                bfm) branch=dev/gpu_drv/bugfix_main ;;
+                opengl) module=$3 ;;
+                debug|release|develop) config=$3 ;;
+            esac
+            shift 
+        done 
 
-        ls $HOME/NVIDIA-Linux-$(uname -m)-*-internal.run | awk -F/ '{print $NF}'  | sort -V 
-        read -p "Enter [version] to continue: " version
-        for nvpid in $(sudo fuser -v /dev/nvidia* 2>/dev/null | grep -v 'COMMAND' | awk '{print $3}' | sort  | uniq); do sudo kill -9 $nvpid; done
-        sudo env IGNORE_CC_MISMATCH=1 IGNORE_MISSING_MODULE_SYMVERS=1 $HOME/NVIDIA-Linux-$(uname -m)-$version-internal.run -s --no-kernel-module-source --skip-module-load || { cat /var/log/nvidia-installer.log; exit 1; }
+        if [[ $module == drivers ]]; then 
+            rsync -ah --progress wanliz@office:/media/wanliz/data/wanliz-sw-gpu-driver-office/$branch/_out/Linux_$(uname -m | sed 's/^x86_64$/amd64/')_$config/NVIDIA-Linux-$(uname -m)-*-internal.run wanliz@office:/media/wanliz/data/wanliz-sw-gpu-driver-office/$branch/_out/Linux_$(uname -m | sed 's/^x86_64$/amd64/')_$config/tests-Linux-$(uname -m).tar $HOME && echo || exit 1
 
-        unset NVTEST_DRIVER NVTEST_DRIVER_BRANCH NVTEST_DRIVER_CHANGELIST NVTEST_DRIVER_DIR && echo "Unset NVTEST_* envvars -> OK"
-        tar -xf $HOME/tests-Linux-$(uname -m).tar -C $HOME 
-        sudo ln -sf $HOME/tests-Linux-$(uname -m)/sandbag-tool/sandbag-tool $HOME/sandbag-tool && echo "Updated: $HOME/sandbag-tool"
+            ls $HOME/NVIDIA-Linux-$(uname -m)-*-internal.run | awk -F/ '{print $NF}'  | sort -V 
+            read -p "Enter [version] to continue: " version
+            for nvpid in $(sudo fuser -v /dev/nvidia* 2>/dev/null | grep -v 'COMMAND' | awk '{print $3}' | sort  | uniq); do sudo kill -9 $nvpid; done
+            sudo env IGNORE_CC_MISMATCH=1 IGNORE_MISSING_MODULE_SYMVERS=1 $HOME/NVIDIA-Linux-$(uname -m)-$version-internal.run -s --no-kernel-module-source --skip-module-load || { cat /var/log/nvidia-installer.log; exit 1; }
+
+            unset NVTEST_DRIVER NVTEST_DRIVER_BRANCH NVTEST_DRIVER_CHANGELIST NVTEST_DRIVER_DIR && echo "Unset NVTEST_* envvars -> OK"
+            tar -xf $HOME/tests-Linux-$(uname -m).tar -C $HOME 
+            sudo ln -sf $HOME/tests-Linux-$(uname -m)/sandbag-tool/sandbag-tool $HOME/sandbag-tool && echo "Updated: $HOME/sandbag-tool"
+        elif [[ $module == opengl ]]; then 
+            rsync -ah --progress wanliz@office:/media/wanliz/data/wanliz-sw-gpu-driver-office/$branch/drivers/OpenGL/_out/Linux_$(uname -m | sed 's/^x86_64$/amd64/')_$config/libnvidia-glcore.so $HOME || exit 1
+            version=$(ls /usr/lib/*-linux-gnu/libnvidia-glcore.so.*  | awk -F '.so.' '{print $2}' | head -1)
+            sudo cp -vf --remove-destination $HOME/libnvidia-glcore.so /usr/lib/$(uname -m)-linux-gnu/libnvidia-glcore.so.$version 
+        fi 
     else
         sudo -H bash -lc "NVTEST_INSTALLER_REUSE_INSTALL=False /mnt/linuxqa/nvt.sh $*" || exit 1
         read -p "ENVVARS (Copy & Paste): " envvars
