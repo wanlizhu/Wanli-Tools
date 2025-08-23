13303774de445372d6a03f782243cab58f79d4fc
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-23 14:48:30 -0700

    s

--- Scripts/NvTest.sh
+++ Scripts/NvTest.sh
@@ -48,7 +48,8 @@ if [[ $1 == driver || $1 == drivers ]]; then
     elif [[ $2 == local ]]; then
         if [[ $3 == *".run" ]]; then 
             for nvpid in $(sudo fuser -v /dev/nvidia* 2>/dev/null | grep -v 'COMMAND' | awk '{print $3}' | sort  | uniq); do 
-                sudo kill -9 $nvpid && echo "Killed $nvpid"
+                echo -n "Killing $nvpid "
+                sudo kill -9 $nvpid && echo "-> OK" || echo "-> Failed"
             done
             sudo env IGNORE_CC_MISMATCH=1 IGNORE_MISSING_MODULE_SYMVERS=1 $3 -s --no-kernel-module-source --skip-module-load || { cat /var/log/nvidia-installer.log; exit 1; }
             unset NVTEST_DRIVER NVTEST_DRIVER_BRANCH NVTEST_DRIVER_CHANGELIST NVTEST_DRIVER_DIR && echo "Unset NVTEST_* envvars -> OK"
