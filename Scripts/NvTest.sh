9b7ab19c1b1fde0aa2d4c932fa0d50296a591208
Wanli Zhu <zhu.wanli@icloud.com>
2025-08-24 17:33:22 -0700

    s

--- Scripts/NvTest.sh
+++ Scripts/NvTest.sh
@@ -166,7 +166,6 @@ elif [[ $1 == env ]]; then
                 chmod 644 ~/.ssh/id_ed25519.pub
             fi 
         fi 
-        bash # With ENVVARS just exported
     fi 
 elif [[ $1 == maxclock ]]; then 
     sudo $HOME/sandbag-tool -unsandbag
