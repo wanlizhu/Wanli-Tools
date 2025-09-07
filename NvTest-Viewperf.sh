1ecc6ccdfd067b95312addac7372555f0006ad00
Wanli Zhu <wanliz@nvidia.com>
2025-09-06 20:23:17 -0700

    Sat Sep  6 08:23:17 PM PDT 2025

--- NvTest-Viewperf.sh
+++ NvTest-Viewperf.sh
@@ -16,7 +16,7 @@ if [[ -z $viewset ]]; then
     for viewset in catia creo energy maya medical snx sw; do 
         ./viewperf/bin/viewperf viewsets/$viewset/config/$viewset.xml $subtest -resolution 3840x2160 &&
         cat results/${viewset//sw/solidworks}*/results.xml
-    fi 
+    done 
 else
     ./viewperf/bin/viewperf viewsets/$viewset/config/$viewset.xml $subtest -resolution 3840x2160 &&
     cat results/${viewset//sw/solidworks}*/results.xml
