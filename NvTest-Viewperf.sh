c1699a145665ecfb65a4785ac1b06fdf8bf61f9c
Wanli Zhu <wanliz@nvidia.com>
2025-09-06 19:58:21 -0700

    Sat Sep  6 07:58:21 PM PDT 2025

--- NvTest-Viewperf.sh
+++ NvTest-Viewperf.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+viewset=$1
+subtest=$2
+
+if [[ -d ~/viewperf2020v3 ]]; then 
+    pushd ~/viewperf2020v3
+elif [[ -d /mnt/linuxqa/wanliz/viewperf2020v3/$(uname -m) ]]; then 
+    pushd /mnt/linuxqa/wanliz/viewperf2020v3/$(uname -m)
+else
+    echo "Error: folder not found ~/viewperf2020v3"
+    exit 1
+fi 
+
+./viewperf/bin/viewperf viewsets/$viewset/config/$viewset.xml $subtest -resolution 3840x2160 &&
+cat results/${viewset//sw/solidworks}*/results.xml
+
+popd >/dev/null 
